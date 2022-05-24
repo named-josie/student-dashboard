7d4d9311087ed53e1bfba0e1e75812e612295fb4
named.josie <named.josie@gmail.com>
2022-05-24 00:48:41 -0400

    updated and add files

--- src/Components/StudentInfo.js
+++ src/Components/StudentInfo.js
@@ -24,19 +24,13 @@ class StudentInfo extends React.Component {
     const { showMore } = this.state
     this.setState({ showMore: !showMore })
   }
-  birthday = (date) => {
-    let formattedDate = new Date(date).toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    }) // https://www.tutorialstonight.com/format-javascript-date.php  found to convert dob
-
-    return formattedDate
-  }
+  
   render() {
+    
     const { student } = this.props
     const { names, username, profilePhoto, dob } = student
     const { showMore } = this.state
+    const dobFormatted = new Date(dob).toDateString().substring(4);
     return (
       <article className='student-gallery'>
         <img src={profilePhoto} alt={names.preferredName} />
@@ -44,7 +38,7 @@ class StudentInfo extends React.Component {
           {names.preferredName} {names.middleName.charAt()}. {names.surname}
         </p>
         <p>{username}</p>
-        <p style={{ color: "purple" }} >Birthday: {this.birthday(dob)}</p>
+        <p style={{ color: "purple" }} >Birthday: {dobFormatted}</p>
         <p className='ontrack'>
           {this.onTrack(student) ? 'On Track to Graduate' : null}
         </p>
